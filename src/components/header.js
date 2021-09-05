3f04cf3cba802fb9dfc95211ac79af9c6c3d8501
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-05 06:23:42 -0400

    Componente del Header maquetado y estilizado

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import '../assets/css/header.css';
+
+class header extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <header className="text-white bg-dark">
+                    <div className="container text-center p-3">
+                        <h1 className="display-2 mb-3"><i>Joy Me <i class="far fa-smile-beam"></i></i></h1>
+                        <p>Photo gallery that should brighten up your day</p>
+                    </div>
+                </header>
+            </React.Fragment>
+        );
+    }
+}
+
+export default header;
