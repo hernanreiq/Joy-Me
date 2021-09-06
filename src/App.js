e002f74d6393481ceb1e95c1423f3445de6a010b
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-06 02:33:43 -0400

    Main maquetado y estilizado, contenido aun no es dinámico

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/header';
+import Main from './components/main';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Main />
     </React.Fragment>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+import "../assets/css/main.css";
+import photo1 from "../assets/img/photo (1).jpg";
+import photo3 from "../assets/img/photo (3).jpg";
+import photo4 from "../assets/img/photo (4).jpg";
+import photo6 from "../assets/img/photo (6).jpg";
+
+class Main extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <div className="white-division"></div>
+                <main>
+                    <div className="container p-5">
+                        <div className="card-columns">
+                            <div className="card shadow">
+                                <img src={photo6} alt="construccion" className="card-img-top" />
+                                <div className="card-body text-justify">
+                                    <p className="card-text">
+                                        Construyendo un hogar
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="card shadow">
+                                <img src={photo1} alt="construccion" className="card-img-top" />
+                                <div className="card-body text-justify">
+                                    <p className="card-text">
+                                        Construyendo un hogar
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="card shadow">
+                                <img src={photo3} alt="construccion" className="card-img-top" />
+                                <div className="card-body text-justify">
+                                    <p className="card-text">
+                                        Protegiendo la naturaleza
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="card shadow">
+                                <img src={photo4} alt="construccion" className="card-img-top" />
+                                <div className="card-body text-justify">
+                                    <p className="card-text">
+                                        Abuela cargando a su nieto
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Main;
