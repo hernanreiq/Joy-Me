35383e3920896e37f989dcefe43c2aafeaa7122d
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-08 17:42:28 -0400

    Impresion de imágenes dinámicamente sin tener que importar una por una, aun sin estilizar

--- src/components/main.js
+++ src/components/main.js
@@ -1,50 +1,41 @@
 import React, { Component } from "react";
 import "../assets/css/main.css";
-import photo1 from "../assets/img/photo (1).jpg";
-import photo3 from "../assets/img/photo (3).jpg";
-import photo4 from "../assets/img/photo (4).jpg";
-import photo6 from "../assets/img/photo (6).jpg";
+import photo1 from '../assets/img/photo (3).jpg';
 
 class Main extends Component {
+    state = {
+        imagesQuantity: 8,
+        all_Images: []
+    }
+    getImagesSRC = () => {
+        var images = [];
+        for (var i = 1; i <= this.state.imagesQuantity; i++){
+            images.push(require(`../assets/img/photo (${i}).jpg`));
+        }
+        this.setState({
+            all_Images: images
+        });
+    }
+    
     render() {
         return (
             <React.Fragment>
                 <div className="white-division"></div>
-                <main>
+                <main onLoad={this.getImagesSRC}>
                     <div className="container p-5">
-                        <div className="card-columns">
-                            <div className="card shadow">
-                                <img src={photo6} alt="construccion" className="card-img-top" />
-                                <div className="card-body text-justify">
-                                    <p className="card-text">
-                                        Construyendo un hogar
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="card shadow">
-                                <img src={photo1} alt="construccion" className="card-img-top" />
-                                <div className="card-body text-justify">
-                                    <p className="card-text">
-                                        Construyendo un hogar
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="card shadow">
-                                <img src={photo3} alt="construccion" className="card-img-top" />
-                                <div className="card-body text-justify">
-                                    <p className="card-text">
-                                        Protegiendo la naturaleza
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="card shadow">
-                                <img src={photo4} alt="construccion" className="card-img-top" />
-                                <div className="card-body text-justify">
-                                    <p className="card-text">
-                                        Abuela cargando a su nieto
-                                    </p>
-                                </div>
-                            </div>
+                        <div id="container_current_image">
+                            <img src={photo1} alt="Testing" />
+                        </div>
+                        <div id="container_carousel_images">
+                            {
+                                this.state.all_Images.map((image, i) => {
+                                    return(
+                                        <div key={i}>
+                                            <img src={image.default} alt="happy carousel"  />
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                 </main>
@@ -54,3 +45,4 @@ class Main extends Component {
 }
 
 export default Main;
+
