97b110e86ef0abb11aa02661d41baf0014b92dcb
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-08 18:47:07 -0400

    Estilizacion de imagenes y caption

---
+++ src/components/image.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css';
+// import 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+
+class Image extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="card bg-transparent" key={this.props.id}>
+                    <img src={this.props.src} alt="Happy Carousel For Joy Me" />
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Image;
--- src/components/main.js
+++ src/components/main.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import "../assets/css/main.css";
-import photo1 from '../assets/img/photo (3).jpg';
+import Image from "./image";
+import photo1 from '../assets/img/photo (1).jpg';
 
 class Main extends Component {
     state = {
         imagesQuantity: 8,
-        all_Images: []
+        all_Images: [],
+        caption_images: ['Invirtiendo en construcción, para mejorar el hogar y aumentar su valor.',
+            'Una gata durmiendo plácidamente.', 'Protegiendo la naturaleza.',
+            'Una abuela felíz de poder cargar a su nieto.', 'Creando un portafolio que sirva de carta de presentación para una persona que quiere mejorar su vida.',
+            'Madre soltera construyendo el hogar de sus tres hijos.', 'Gato rubio en un hogar.', 'Foto de graduación de un estudiante universitario.']
     }
     getImagesSRC = () => {
         var images = [];
-        for (var i = 1; i <= this.state.imagesQuantity; i++){
+        for (var i = 1; i <= this.state.imagesQuantity; i++) {
             images.push(require(`../assets/img/photo (${i}).jpg`));
         }
         this.setState({
             all_Images: images
         });
     }
-    
+
     render() {
         return (
             <React.Fragment>
@@ -26,13 +31,17 @@ class Main extends Component {
                         <div id="container_current_image">
                             <img src={photo1} alt="Testing" />
                         </div>
-                        <div id="container_carousel_images">
+                        <div className="container_caption">
+                            <p className="caption">{this.state.caption_images[0]}</p>
+                        </div>
+                        <div id="container_carousel_images" className="card-columns">
                             {
                                 this.state.all_Images.map((image, i) => {
-                                    return(
-                                        <div key={i}>
-                                            <img src={image.default} alt="happy carousel"  />
-                                        </div>
+                                    return (
+                                        <Image
+                                            id={i}
+                                            src={image.default}
+                                        />
                                     )
                                 })
                             }
